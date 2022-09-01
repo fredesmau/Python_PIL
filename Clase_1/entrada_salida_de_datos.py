658af2b771303a068dea7b24fc60a9139e35ecb4
fredesmau <fredesmau@gmail.com>
2022-09-01 19:38:48 -0300

    modificado entrada y salida de datos

---
+++ Clase_1/entrada_salida_de_datos.py
@@ -0,0 +1,26 @@
+print("Hola Mundo...\n")
+print("Desde'\t visual")
+
+# Otras funciones del print
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+
+# Salto de línea
+print("Esto es un salto \n de línea")
+
+# Tabulación
+print("Con esto se \t tabula")
+
+#Concatenar
+print("Hola" + "Mundo")
+print("Hola", "Mundo", sep='+')
+
+# End
+print("Hola", "Mundo", end='\t')
+print("Desde visual")
+
+# Entrada de datos
+input('Ingrese un mensaje: ')
+
